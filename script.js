c8f61703c68e7bf0333f9eeaa4699bfcdc2e17cc
Manish Gupta <59307835+manishbittu0999@users.noreply.github.com>
2022-05-21 12:07:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -65,7 +65,7 @@ function loadMovieDetails(){
         movie.addEventListener('click', async () => {
             searchList.classList.add('hide-search-list');
             movieSearchBox.value = "";
-            const result = await fetch(`http://www.omdbapi.com/?i=${movie.dataset.id}&apikey=62f3389e`);
+            const result = await fetch(`https://www.omdbapi.com/?i=${movie.dataset.id}&apikey=62f3389e`);
             const movieDetails = await result.json();
             displayMovieDetails(movieDetails);
         });
